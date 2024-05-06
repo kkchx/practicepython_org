8750d4db82c11b0406701b50bbbf446cd82d7bf2
cryptoracing <101093104+cryptoracing@users.noreply.github.com>
2024-05-06 21:08:07 +0700

    solution 8 - 9, test 8

--- 006_Palyndrome/solution1.py
+++ 006_Palyndrome/solution1.py
@@ -1,5 +1,15 @@
 def palindrome_checker(string_to_check):
-    return 1
+    result = 1
+    for i in range (0,int(len(string_to_check)/2)):
+        if string_to_check[i] != string_to_check[len(string_to_check)-i-1]:
+            result -= 1
+            break
+    return result
 
 if __name__ == '__main__':
-    te
+    while True:
+        test = input("Input a string to check\n")
+        if palindrome_checker(test) == 1:
+            print("It's a Palindrome")
+        else:
+            print("It's Not a Palindrome")
