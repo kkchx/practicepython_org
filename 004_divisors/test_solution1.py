25334b834d259c15e125520543fe0b9ae72b046d
cryptoracing <101093104+cryptoracing@users.noreply.github.com>
2024-05-09 16:07:26 +0700

    test correction for Task 4

--- 004_divisors/test_solution1.py
+++ 004_divisors/test_solution1.py
@@ -1,32 +1,15 @@
 import unittest
+from solution2 import find_divisors
 
 class MyTestCase(unittest.TestCase):
     def test_num(self):
-        num = 2
-        list_range = list(range(1, num + 1))
-        divisor_list = []
-        for number in list_range:
-            if num % number == 0:
-                divisor_list.append(number)
-        self.assertEqual(divisor_list, [1, 2])
+        self.assertEqual(find_divisors(3), [1, 3])
 
     def test_num1(self):
-        num2 = 3
-        list_range = list(range(1, num2 + 1))
-        divisor_list = []
-        for number in list_range:
-            if num2 % number == 0:
-                divisor_list.append(number)
-        self.assertEqual(divisor_list, [1, 3])
+        self.assertEqual(find_divisors(16), [1, 2, 4, 8, 16])
 
     def test_num2(self):
-        num3 = "a"
-        list_range = list(range(1, num3 + 1))
-        divisor_list = []
-        for number in list_range:
-            if num3 % number == 0:
-                divisor_list.append(number)
-        self.assertEqual(divisor_list, "a")
+        self.assertEqual(find_divisors(0), [])
 
 if __name__ == '__main__':
     unittest.main()
